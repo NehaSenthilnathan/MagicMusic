3ce55d6155d1cc44b4eecbee9d9be1a68074ee95
NehaSenthilnathan <88185338+NehaSenthilnathan@users.noreply.github.com>
2023-02-23 13:05:12 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+song1 = "";
+song2 = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
+
 function setup(){
     canvas.createCanvas (550, 600);
     canvas.center();
@@ -6,13 +13,25 @@ function setup(){
     video.hide();
 
     poseNet = ml5.poseNet(video, modelLoaded);
-    poseNet.on ('pose', gotPose);
+    poseNet.on ('pose', gotPoses);
 }
-
 
 function modelLoaded(){
     console.log("PoseNet is initialized");
 }
+
+function gotPoses(results){
+    if (results.length > 0){
+    
+        leftWristX = results[0].pose.leftWrist.x;
+    leftWristY = results[0].pose.leftWrist.y;
+    console.log("leftWristX = " +leftWristX+ "leftWristY" +leftWristY);
+
+    rightWristX = results[0].pose.rightWrist.x;
+    rightWristY = results[0].pose.rightWrist.y;
+    console.log("rightWristX = " +rightWristX+ "rightWristY" +rightWristY);
+}
+    }
 
 function preLoad(){
     song1 = loadSound("Charlie Puth - Light Switch.mp3");
@@ -20,7 +39,14 @@ function preLoad(){
 }
 
 function play(){
-    song.play();
+    song1.play();
+    song1.setVolume(1);
+    song1.rate(1);
+    
+    song2.play();
+    song2.setVolume(1);
+    song2.rate(1);
+    
 }
 
 function draw(){
